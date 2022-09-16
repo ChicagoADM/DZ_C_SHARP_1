9a94154474ec769a342b72e444af37844912aa93
ChicagoADM <6974420@gmail.com>
2022-09-16 15:45:33 +0300

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число больше '0': ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 2;
+while(num2 <= num)
+{
+    Console.Write(num2 + ", ");
+    num2 = num2 + 2;
+}
